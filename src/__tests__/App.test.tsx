f4816c3a45cf04d66bd9fbfc20baf99f1db2ee0b
AgonIdrizi <agon.88@hotmail.com>
2021-02-25 15:12:37 +0100

    add and configure jest, react-testing-library

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+
+
+describe("", () => {
+  it("afo", () => {
+    const container = render(<h2>Test</h2>)
+    expect(1).toBe(1)
+  })
+})
