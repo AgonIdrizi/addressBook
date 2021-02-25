d546e11ab5d449e1f5089637ffc31db03db704c5
AgonIdrizi <agon.88@hotmail.com>
2021-02-25 16:33:11 +0100

    add react-router, create folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom'
+import {Switch} from 'react-router';
+import Home from './screens/Home/Home'
+import Settings from './screens/Settings/Settings'
 import './styles.scss'
 
-const App = () => {
-  return(
-    <h1>My React and TypeScript App!{" "}
-  </h1>
-  )
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/settings" component={Settings} />
+      <Route path="*" render={() => <Redirect to="/" push />} />
+    </Switch>
+  </Router>
+)
   
-};
-
+  
 ReactDOM.render(
   <React.StrictMode>
     <App />
