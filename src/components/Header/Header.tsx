dbfba892392a4d2fb57cb5e2b4fcd840b2dddf37
AgonIdrizi <agon.88@hotmail.com>
2021-02-26 12:59:15 +0100

    change Header items position based on route

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import NavLink from '../UI/NavLink/NavLink';
 import './Header.scss';
 
 const Header: React.FunctionComponent = (): JSX.Element => {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === '/';
+
   return (
-    <nav className="Header">
-      <NavLink linkTo="/">Back Home</NavLink>
-      <NavLink linkTo="/settings">Settings</NavLink>
+    <nav className={`Header ${isHomePage ? 'Flex-end' : ''}`}>
+      {isHomePage ? (
+        <NavLink linkTo="/settings">Settings</NavLink>
+      ) : (
+        <NavLink linkTo="/">Back Home</NavLink>
+      )}
     </nav>
   );
 };
